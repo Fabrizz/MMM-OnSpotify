3b45973fd29e4f6f399dbc77e5e2e9c0cff0baac
Fabrizio <65259076+Fabrizz@users.noreply.github.com>
2024-11-25 17:22:35 -0300

    Remove headers on Canvas

--- utils/SpotifyFetcher.js
+++ utils/SpotifyFetcher.js
@@ -208,6 +208,7 @@ module.exports = class SpotifyFetcher {
           );
           return new Error("Error refreshing access token")
         }
+
         return res.json();
       })
       .catch((error) => {
@@ -277,17 +278,17 @@ module.exports = class SpotifyFetcher {
     if (currentTime < this.canvasTokenExpiresAt) {
       return this.canvasToken
     } else {
-      const ccc = {
+      let ccc = {
         headers: {
-          "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0",
-          Accept:
-            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-          "Accept-Language": "en-US,en;q=0.5",
-          "Alt-Used": "open.spotify.com",
-          "Upgrade-Insecure-Requests": "1",
-          "Sec-Fetch-Dest": "document",
-          "Sec-Fetch-Mode": "navigate",
-          "Sec-Fetch-Site": "cross-site",
+          //"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0",
+          //Accept:
+          //  "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+          //"Accept-Language": "en-US,en;q=0.5",
+          //"Alt-Used": "open.spotify.com",
+          //"Upgrade-Insecure-Requests": "1",
+          //"Sec-Fetch-Dest": "document",
+          //"Sec-Fetch-Mode": "navigate",
+          //"Sec-Fetch-Site": "cross-site",
         }
       }
       if (this.credentials.experimentalCanvasSPDCookie.length > 0) {
